ad7e23fa9bb1287adf861a0406f7df1c7e7a6734
Stefan H. Holek <stefan@naturalvision.de>
2021-01-14 23:39:07 +0100

    Make the projector installable

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-# Install PyTorch extensions
+# Install PyTorch extensions and projector
 # pip install .
 
 from setuptools import setup
 from torch.utils import cpp_extension
 
 setup(
-    name='stylegan2-pytorch-ops',
+    name='stylegan2-pytorch',
     version='1.0.0',
     ext_modules=[
         cpp_extension.CppExtension(
@@ -16,4 +16,17 @@ setup(
     cmdclass={
         'build_ext': cpp_extension.BuildExtension,
     },
+    py_modules=[
+        'model',
+        'projector',
+    ],
+    packages=[
+        'lpips',
+        'op',
+    ],
+    zip_safe=False,
+    include_package_data=True,
+    author='Stefan H. Holek',
+    author_email='stefan@naturalvision.de',
+    url='https://github.com/naturalvision/stylegan2-pytorch',
 )
